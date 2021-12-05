a649997f562d2c1f3c7b90cae73fe470d46a73a3
Mohammad Aakash <muhammadaakash249@gmail.com>
2021-12-05 16:16:13 +0500

    correcting the spellings of internship prop

--- src/learn.js
+++ src/learn.js
@@ -5,7 +5,7 @@ function Learn(props){
         <div>
             <h1>I am Learning {props.serverLess} Technology </h1>
             <h1>I am also Learning {props.serverlessAI} Technology too.</h1>
-            <h1>Now a days am doing an Internship as a {props.intership}</h1>
+            <h1>Now a days am doing an Internship as a {props.internship}</h1>
         </div>
     )
 }
