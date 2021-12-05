df6b12d65917b544d8394df5090770e980547739
Mohammad Aakash <muhammadaakash249@gmail.com>
2021-12-05 15:48:37 +0500

    Just created some Child Component in React
    Practicing Work of Class 3rd of Bootcamp 2020, and done some parent and child component functionality

---
+++ src/learn.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Learn(props){
+    return(
+        <div>
+            <h1>I am Learning {props.serverLess} Technology </h1>
+            <h1>I am also Learning {props.serverlessAI} Technology too.</h1>
+        </div>
+    )
+}
+
+export default Learn;
